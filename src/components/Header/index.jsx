c1b7cca12f9d19f932dce46eff6dec2816f52932
AlexSuspect13 <pmodelist@bk.ru>
2022-01-04 00:32:51 +0300

    start project

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './Header.modules.scss'
+
+export default Header = ()=>{
+    return(
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
